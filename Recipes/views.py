8900da63f3a418723a8e090cf51eb9ceb39fd19d
Bruno Henrique Oliveira <bh084996074995@gmail.com>
2024-03-05 20:46:41 -0300

    Dividindo a home em arquivos separados

--- Recipes/views.py
+++ Recipes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html') # namespace acompanhando na url 
+    return render(request, 'recipes/pages/home.html') # namespace acompanhando na url 
