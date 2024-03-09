b2f37a8c898fed8ad29216a4a66921b20866ac7d
Bruno Henrique Oliveira <bh084996074995@gmail.com>
2024-03-09 12:34:24 -0300

    diferenciando list page de detail page

--- Recipes/views.py
+++ Recipes/views.py
@@ -6,5 +6,7 @@ def home(request):
                   {'recipes': [make_recipe() for _ in range(10)]})
 
 def recipes(request, id):
-    return render(request, 'recipes/pages/recipes-view.html', context=
-                  {'recipe': make_recipe()})
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
